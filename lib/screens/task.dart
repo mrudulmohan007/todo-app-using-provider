c8baa479cd6957009ecb901fcb7373ed0abbdadc
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-09-11 19:20:20 +0530

    refactored listview & listtile widgets

--- lib/screens/task.dart
+++ lib/screens/task.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/task_list.dart';
+import 'package:todo_app/widgets/task_tile.dart';
 
 class TaskScreen extends StatelessWidget {
   const TaskScreen({super.key});
@@ -7,38 +9,70 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 60,
-          left: 30,
-          right: 30,
-          bottom: 30,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 32,
-                color: Colors.lightBlueAccent,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 60,
+              left: 30,
+              right: 30,
+              bottom: 30,
             ),
-            SizedBox(
-              height: 10.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 32,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'QuickDo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'TaskTrek',
-              style: TextStyle(
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontSize: 50,
-                fontWeight: FontWeight.w700,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(
+                    20.0,
+                  ),
+                  topRight: Radius.circular(20.0),
+                ),
               ),
+              child: TasksList(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
